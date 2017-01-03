1e6b4463576677cf3d47535fc114cf13449a0199
faorfwd <faor.fwd@gmail.com>
2017-01-03 21:06:33 +0100

    Add files via upload

--- src/main/groovy/com/entagen/jenkins/JenkinsApi.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsApi.groovy
@@ -24,7 +24,7 @@ class JenkinsApi {
         this.jenkinsServerUrl = jenkinsServerUrl
         this.restClient = new RESTClient(jenkinsServerUrl)
 		def aURL = new URL(this.jenkinsServerUrl)
-		def rootUrl = aURL.getProtocol()+":"+aURL.getAuthority()+"/"
+		def rootUrl = aURL.getProtocol()+"://"+aURL.getAuthority()+"/"
 		println rootUrl
 		this.rootRestClient = new RESTClient(rootUrl)
 	}
